dbf167d3496bdb0f8f2b47a59978430714a525cd
Siansiehi Baye <armelbaye1984@live.com>
2020-04-03 19:15:56 -0400

    first commit

---
+++ src/main/java/com/bayegroup/yasisolutions/YasisolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.bayegroup.yasisolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YasisolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YasisolutionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bayegroup/yasisolutions/YasisolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayegroup.yasisolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YasisolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
